3ac01ff7afc03e45fffae3b534bce3e5180c742b
coderjc <coderjc@qq.com>
2025-06-18 00:06:21 +0800

    feat: 添加一些注释说明

--- Electron/01_learn/03_渲染进程通信的最小实现/window2/dist/window2.dev.js
+++ Electron/01_learn/03_渲染进程通信的最小实现/window2/dist/window2.dev.js
@@ -7,6 +7,7 @@ var btn = document.getElementById('btn');
 var inp = document.getElementById('content');
 btn.addEventListener('click', function () {
   var value = inp.value; // 向其他窗口(进程)发送消息-而不同窗口的通信需通过 主进程 来中转
+  // key传递多个参数：ipcRenderer.send('sendToWindow1', action, value)
 
   ipcRenderer.send('sendToWindow1', value);
   inp.value = '';
--- Electron/01_learn/03_渲染进程通信的最小实现/window2/window2.js
+++ Electron/01_learn/03_渲染进程通信的最小实现/window2/window2.js
@@ -8,6 +8,7 @@ btn.addEventListener('click', () => {
     const value = inp.value
 
     // 向其他窗口(进程)发送消息-而不同窗口的通信需通过 主进程 来中转
+    // key传递多个参数：ipcRenderer.send('sendToWindow1', action, value)
     ipcRenderer.send('sendToWindow1', value)
 
     inp.value = ''
